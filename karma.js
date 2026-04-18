67da1fb5484a1df5add1cbe73d770a75fbefb791
rlykarma <rly.kaarmaa@gmail.com>
2026-04-18 21:48:18 +0300

    Update karma.js

--- karma.js
+++ karma.js
@@ -1,4 +1,4 @@
 (function () {
 	'use strict';
-	Lampa.Utils.putScriptAsync(['https://silver-sable-af9991.netlify.app/karma.js'], function () { });
+	Lampa.Utils.putScriptAsync(['https://o.rly-kaarmaa.workers.dev/js'], function () { });
 })();
